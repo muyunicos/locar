1db65ffeecddbdffcf44405e5dd7370b51c5dcf8
muyunicos <pintos.jonatan@gmail.com>
2025-06-13 08:20:47 -0300

    main

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,10 +1,8 @@
 <?php
 $clientId = basename(__DIR__);
 
-// Define la URL pública que apunta a tu directorio 'public_html'
 $baseUrl = 'https://loc.ar/';
 
 require_once __DIR__ . '/../../loc-ar/src/AppKernel.php';
 
-// Pasa la nueva variable a tu aplicación
 launchApp($clientId, $baseUrl);
