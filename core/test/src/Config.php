51004be72c5d31b003b394178c03aec9a5665432
muyunicos <pintos.jonatan@gmail.com>
2025-06-20 14:33:16 -0300

    main 1.2

--- core/test/src/Config.php
+++ core/test/src/Config.php
@@ -1,5 +1,8 @@
 <?php
+    if (!defined('CLIENT_ID')) {
+            define('CLIENT_ID', basename(dirname($_SERVER['DOCUMENT_ROOT'])));
+        }
+    define ('CLIENT_PATH', defined('DEV_BRANCH') && DEV_BRANCH ? dirname(__DIR__,3) . '/public_html/' . CLIENT_ID : dirname(__DIR__,2) . '/public_html/' . CLIENT_ID);    
     define ('CLIENT_URL', "https://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), '/\\'));
-    define ('CLIENT_PATH', defined('DEV_BRANCH') && DEV_BRANCH ? dirname(__DIR__,3) . '/public_html/' . CLIENT_ID : dirname(__DIR__,2) . '/public_html/' . CLIENT_ID);
     define ('PUBLIC_URL', 'https://' . implode('.', array_slice(explode('.', parse_url(CLIENT_URL, PHP_URL_HOST) ?? ''), -2)) . '/app' );
     define ('PRIVATE_PATH', dirname(__DIR__));
