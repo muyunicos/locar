7f033b08e38a5c5a874ef00241595949afce0c4b
muyunicos <pintos.jonatan@gmail.com>
2025-06-21 13:35:04 -0300

    main 1.4

--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,5 +1,5 @@
 <?php
 	define('CLIENT_ID', basename(__DIR__));
-	define('DEV_BRANCH', 'test');
+	define('DEV_BRANCH', 'v0.1');
 	require_once(defined('DEV_BRANCH') && DEV_BRANCH ? dirname(__DIR__,2) . '/core/' . DEV_BRANCH . '/src/AppKernel.php' : dirname(__DIR__,2) . '/core/src/AppKernel.php');
 	launchApp();
